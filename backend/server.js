aa699aaa7ae6c90e91a25eb475912e066575d7b4
Cynthia MOSBAHI <cynthia.mosbahi@D-PARDAILHAN.local>
2026-04-16 15:16:31 +0200

    Suppression du mot de passe en clair dans server.js

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,21 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 
+require('dotenv').config();
+
 const app = express();
 app.use(cors());
 app.use(express.json()); // Indispensable pour lire le body de React
 
 // 1. Connexion à PostgreSQL
+
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'steelfit',
-  password: '123456789', 
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // 2. Route d'accueil
